a2a76906adae90c5118da1c3d4a37e5d6a6f03f1
devramirez <aramirezdev1@gmail.com>
2023-08-24 03:33:25 -0400

    Defined columns under product

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,7 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    // presents as id, category_name, and associated tags according to documentation
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
